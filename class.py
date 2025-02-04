fb0c68aa2827103b86153a7af0219dd840a957fa
KavishSingla <singlakavish09@gmail.com>
2025-02-04 18:13:41 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,30 @@
+# import chitkaramaths
+# chitkaramaths.sum(5,6)
+
+# chitkaramaths.product(5,6)
+
+
+# print(chitkaramaths.x)
+
+# -----------------------------------------------------------------------------------------------------------------
+
+# import chitkaramaths as cm
+# cm.sum(5,6)
+
+# cm.product(5,6)
+
+
+# print(cm.x)
+
+# -----------------------------------------------------------------------------------------------------------------
+
+# from chitkaramaths import x, add
+
+# print(x)
+# add(10, 20)
+# product(10, 20) # NameError: name “product is not defined”
+
+# -----------------------------------------------------------------------------------------------------------------
+
+import chitkaramaths
+print(dir())
